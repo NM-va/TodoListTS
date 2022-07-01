57d6c5991785435b66bd2dbca554a5497427b622
NM-va <nad.n.mwa@gmail.com>
2022-07-02 00:32:45 +0300

    replacement createStore to configureStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
-import {applyMiddleware, combineReducers, createStore} from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import {appReducer} from './app-reducer'
-import {authReducer} from '../features/Login/auth-reducer'
+import {applyMiddleware, combineReducers, createStore} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+import {authReducer} from '../features/Login/auth-reducer';
+import {configureStore} from '@reduxjs/toolkit';
+import { appReducer } from './app-reducer';
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -12,11 +14,20 @@ const rootReducer = combineReducers({
     todolists: todolistsReducer,
     app: appReducer,
     auth: authReducer
-})
+});
 // непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+// export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+export type RootReducerType = typeof rootReducer;
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().prepend(thunkMiddleware)
+});
+
 // определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<typeof rootReducer>
+export type AppRootStateType = ReturnType<RootReducerType>
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
